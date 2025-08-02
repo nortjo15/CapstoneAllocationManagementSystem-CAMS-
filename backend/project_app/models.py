2cfeda215157fa5607678b3ab4b0b510b6754a7b
Muhammad Umer <muhammad.umer1@student.curtin.edu.au>
2025-08-03 00:03:57 +0800

    feat(project): add Project model and mirgate

--- backend/project_app/models.py
+++ backend/project_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+# Project Information
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True) #Automatic PK, increments
+    title = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=True, blank=True)
+    capacity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return f"{self.project_id} - {self.title}"
