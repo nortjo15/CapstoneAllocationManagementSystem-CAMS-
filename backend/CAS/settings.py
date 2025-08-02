b5d2544be378630814835dd434e2afde127d7953
Muhammad Umer <muhammad.umer1@student.curtin.edu.au>
2025-08-02 22:31:48 +0800

    feat(config): add media file handling for student attachments

--- backend/CAS/settings.py
+++ backend/CAS/settings.py
@@ -138,3 +138,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files for storing Student attachments 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
