2aeb5aed7004bd9e6fda549d01213ddab9717ee3
Muhammad Umer <muhammad.umer1@student.curtin.edu.au>
2025-08-02 22:19:22 +0800

    feat(student): define Student model with relevant information and file attachments

--- backend/student_app/models.py
+++ backend/student_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+# Student Information 
+# StudentID is the primary key 
+# name, major, application_submitted not allowed to be null 
+# cwa, email, notes, cv, resume allowed to be null fields 
+class Student(models.Model):
+    student_id = models.charField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    cwa = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        null=True,
+        blank=True
+    )
+    major = models.CharField(max_length=100, null=False)
+    application_submitted = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, null=True)
+    notes = models.TextField(null=True, blank=True)
+    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+    cv = models.FileField(upload_to='cvs/', null=True, blank=True)
+    # Settings.py should configure media settings 
+
+    def __str__(self):
+        return f"{self.student_id} - {self.name}"
