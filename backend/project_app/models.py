9db8b7da7b1d3d1b150cd5878b4165c19f3dc9b0
Muhammad Umer <muhammad.umer1@student.curtin.edu.au>
2025-08-03 15:05:09 +0800

    (feat): Add ProjectPreference and GroupPreference models with migrations

--- backend/project_app/models.py
+++ backend/project_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from student_app.models import Student
 from django.core.validators import MinValueValidator
 
 # Project Information
+# Primary key is configured and incremented automatically 
+# Title & Description can be null, Capacity cannot be
 class Project(models.Model):
     project_id = models.AutoField(primary_key=True) #Automatic PK, increments
     title = models.CharField(max_length=200, null=False)
@@ -10,3 +13,17 @@ class Project(models.Model):
 
     def __str__(self):
         return f"{self.project_id} - {self.title}"
+    
+# Stores information about a Student's Project Preference
+# Foreign Key to Student & Project
+class ProjectPreference(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    rank = models.PositiveBigIntegerField()
+
+    class Meta: 
+        unique_together = ('student', 'rank') # multiple projects can't be given the same rank
+        ordering = ['rank']
+
+    def __str__(self):
+        return f"{self.student} - {self.project} (Rank {self.rank})"
--- backend/student_app/models.py
+++ backend/student_app/models.py
@@ -25,3 +25,22 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.student_id} - {self.name}"
+    
+# Stores information on a Student's Group Preferences 
+# Can access who a student has preferred or avoided (given preferences)
+# Can access who has preferred or avoided this student (recieved preferences)
+class GroupPreference(models.Model):
+    PREFERENCE_TYPES = [
+        ('like', 'Would Like to Work With'),
+        ('avoid', 'Would Prefer to Avoid'),
+    ]
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='given_preferences')
+    target_student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='recieved_preferences')
+    preference_type = models.CharField(max_length=10, choices=PREFERENCE_TYPES)
+
+    class Meta:
+        unique_together = ('student', 'target_student')
+
+    def __str__(self):
+        return f"{self.student} → {self.target_student} ({self.preference_type})"
