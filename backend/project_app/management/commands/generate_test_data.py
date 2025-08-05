4607a42078457ae4d4a99dcd10e7f5bcf6f8f994
Muhammad Umer <muhammad.umer1@student.curtin.edu.au>
2025-08-05 22:35:30 +0800

    feat(sample-data): Generated sample student data

--- backend/project_app/management/commands/generate_test_data.py
+++ backend/project_app/management/commands/generate_test_data.py
@@ -1,6 +1,9 @@
+import csv 
+import os 
+from django.conf import settings
 from django.core.management.base import BaseCommand
-from student_app import Student, GroupPreference 
-from project_app import (
+from student_app.models import Student, GroupPreference 
+from project_app.models import (
     Project,
     ProjectPreference, 
     SuggestedGroup, 
@@ -9,3 +12,47 @@ from project_app import (
     FinalGroupMember
 )
 
+# Generate test data from specified or default file 
+# - Stored in project_app/fixtues 
+class Command(BaseCommand):
+    help = "Generate test data"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--path',
+            type=str,
+            help='Path to CSV file to import',
+            default=os.path.join(settings.BASE_DIR, 'project_app', 'fixtures', 'test_students.csv')
+        )
+
+
+    def handle(self, *args, **options):
+        file_path = options['path']
+
+        if not os.path.exists(file_path):
+            self.stdout.write(self.style.ERROR(f"File not found: {file_path}"))
+            return 
+        
+        with open(file_path, newline='', encoding='utf-8') as csvfile: 
+            reader = csv.DictReader(csvfile)
+            count = 0
+
+            for row in reader: 
+                student_id = row['student_id']
+                name = row['name']
+                cwa = row['cwa']
+                major = row['major']
+
+                student, created = Student.objects.update_or_create(
+                    student_id=student_id,
+                    defaults={
+                        'name': name,
+                        'cwa': cwa if cwa else None, 
+                        'major': major, 
+                        'application_submitted': False
+                    }
+                )
+                if created: 
+                    count += 1 
+
+        self.stdout.write(self.style.SUCCESS(f"Successfully imported or updated {count} students."))
