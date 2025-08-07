b87d03dedbb6f84781719ddd254792a0e88bcd49
Jonny Taylor <103164779+Jonnymtaylor7@users.noreply.github.com>
2025-08-07 17:13:49 +0800

    feat (studentForm) Form has been completed with all necessary fields. However does not fetch data correctly and cannot yet be submitted

--- backend/student_app/views.py
+++ backend/student_app/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from django.shortcuts import render
 from .models import Student, GroupPreference
 from .serializers import StudentSerializer, GroupPreferenceSerializer
+from project_app.models import Project
+from project_app.serializers import ProjectSerializer
 
 class StudentListCreateView(generics.ListCreateAPIView):
     queryset = Student.objects.all()
@@ -15,6 +17,10 @@ class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
 class GroupPreferenceListCreateView(generics.ListCreateAPIView):
     queryset = GroupPreference.objects.all()
     serializer_class = GroupPreferenceSerializer
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 def student_form(request):
     return render(request, "student_form.html")
