eb40b399693d75cdf43011de2771eaabcb5c16ef
Muhammad Umer <muhammad.umer1@student.curtin.edu.au>
2025-08-14 19:52:23 +0800

    fix(ImportStudents): Modal is functional

--- backend/admin_app/static/admin_app/js/import_students.js
+++ backend/admin_app/static/admin_app/js/import_students.js
@@ -1,7 +1,7 @@
 //Get form elements
 const import_form = document.getElementById('importStudentForm');
 const import_modal = document.getElementById('importModal')
-const import_closeBtn = import_modal.querySelector('close-btn')
+const import_closeBtn = import_modal.querySelector('.close-btn')
 const import_modalSubmit = import_form.querySelector('button[type="submit"]');
 
 function openImportModal()
@@ -12,5 +12,5 @@ function openImportModal()
 
 import_closeBtn.onclick = () => import_modal.style.display = 'none';
 window.onclick = (e) => {
-    if (e.target == import_modal) modal.style.display = 'none';
+    if (e.target == import_modal) import_modal.style.display = 'none';
 }
