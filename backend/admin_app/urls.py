8376dd47d1abef6399097887dc4e31f8493e6285
Jonathan Taylor <jonathan.m.taylor@student.curtin.edu.au>
2025-08-18 12:17:59 +0800

    dev(views): Created group_views.py in the view directory. Updated corresponding imports and URLs, tested pre-existing functionality. All functional

--- backend/admin_app/urls.py
+++ backend/admin_app/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings 
-from django.conf.urls.static import static 
 from .import views
 from django.urls import path
 from .views import *
@@ -26,25 +25,32 @@ from .view.project_views import ProjectListCreateView, ProjectPreferenceListCrea
 from .view.auth_views import register_view, login_view, logout_view, login_success
 from .view.admin_views import AdminLogListCreateView
 from .view.round_views import round_view
+from .view.group_views import SuggestedGroupListCreateView, SuggestedGroupMemberListCreateView, FinalGroupListCreateView, FinalGroupMemberListCreateView
 from .models import *
  
 urlpatterns = [
+    #Auth_views
     path('register/', register_view, name="register"),
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
     path('login_success/', login_success, name="login_success"),
-    #path('test/', views.test_view, name="test"),
+    #Admin_views
     path('admin/logs/', AdminLogListCreateView.as_view()),
-    path('student_view/', views.student_view, name='student_view'),
-    path('settings/', views.settings_view, name='settings'),
     path('send-notification/', SendNotificationView.as_view(), name='send_notification'),
+    #Student_views
+    path('student_view/', views.student_view, name='student_view'),
     path('students/create/', views.student_create, name='admin_student_create'),
     path('students/import/', views.admin_student_import, name='admin_student_import'),
+    #Project_views
     path('projects/', ProjectListCreateView.as_view()),
     path('preferences/', ProjectPreferenceListCreateView.as_view()),
+    #Group_views
     path('suggested/', SuggestedGroupListCreateView.as_view()),
     path('suggested/members/', SuggestedGroupMemberListCreateView.as_view()),
     path('final/', FinalGroupListCreateView.as_view()),
     path('final/members/', FinalGroupMemberListCreateView.as_view()),
+    #Settings_views
+    path('settings/', views.settings_view, name='settings'),
+    #Round_views
     path('rounds/', round_view, name='round_view'),
 ]
--- backend/admin_app/views.py
+++ backend/admin_app/views.py
@@ -23,25 +23,6 @@ from .serializers import *
 
 
 
-def round_view(request):
-   return render(request, "rounds.html")
-
-class SuggestedGroupListCreateView(generics.ListCreateAPIView):
-    queryset = SuggestedGroup.objects.all()
-    serializer_class = SuggestedGroupSerializer
-
-class SuggestedGroupMemberListCreateView(generics.ListCreateAPIView):
-    queryset = SuggestedGroupMember.objects.all()
-    serializer_class = SuggestedGroupMemberSerializer
-
-class FinalGroupListCreateView(generics.ListCreateAPIView):
-    queryset = FinalGroup.objects.all()
-    serializer_class = FinalGroupSerializer
-
-class FinalGroupMemberListCreateView(generics.ListCreateAPIView):
-    queryset = FinalGroupMember.objects.all()
-    serializer_class = FinalGroupMemberSerializer
-
 
 # Adding a basic students view page 
 # Ensures only authenticated users can access it 
