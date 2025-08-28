7b4cf1ac86c329456866abc9935a4a826ff83a6e
Matthew <matthew.henderso@student.curtin.edu.au>
2025-08-28 23:55:22 +0800

    chore(git): Missed one merge conflict

--- backend/admin_app/view/round_views.py
+++ backend/admin_app/view/round_views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-<<<<<<< HEAD
-# @login_required
-=======
 @login_required
->>>>>>> merge-resolver/sprint-2
 def round_view(request):
    return render(request, "rounds.html")
