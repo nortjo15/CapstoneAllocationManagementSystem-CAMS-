95eca5a0a871c7f644b8672106a1ef0b0e1a65da
Jonathan Edwards <20593593@student.curtin.edu.au>
2025-09-15 14:47:07 +0800

    Refactor(capstone information sections): Admins shouldnt be able to change them. Should just have default ones

--- backend/admin_app/models.py
+++ backend/admin_app/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models # type: ignore
 from django.contrib.auth.models import User 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
