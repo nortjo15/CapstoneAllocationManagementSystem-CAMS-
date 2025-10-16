9c5ca5373388f050d84df614af72d32d469ca311
Matthew <matthew.henderso@student.curtin.edu.au>
2025-10-17 00:21:40 +0800

    dev(settings): added export for final groups and suggested groups

--- backend/admin_app/view/admin_views.py
+++ backend/admin_app/view/admin_views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core import management
+from admin_app.models import FinalGroup, SuggestedGroup
+import json
 from django.http import HttpResponse
 #from django.contrib.admin.views.decorators import staff_member_required
 from io import StringIO
@@ -33,5 +35,50 @@ def download_db(request):
 
     filename = f"db_backup_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
     response = HttpResponse(data, content_type='application/json')
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+    return response
+
+@login_required
+def download_final_groups(request):
+    #Build data
+    data = []
+    for group in FinalGroup.objects.prefetch_related('members__student').all():
+        data.append({
+            'group_name': group.name or f"FinalGroup {group.finalgroup_id}",
+            'project_title': group.project.title,
+            'students': [
+                {'name': m.student.name, 'email': getattr(m.student, 'email', '')} 
+                for m in group.members.all()
+            ]
+        })
+
+    json_data = json.dumps(data, indent=2)
+
+    filename = f"final_groups_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
+
+    response = HttpResponse(json_data, content_type='application/json')
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+    return response
+
+@login_required
+def download_suggested_groups(request):
+    # Build data
+    data = []
+    for group in SuggestedGroup.objects.prefetch_related('members__student').all():
+        project_title = group.project.title if group.project else None  # <-- safe check
+        data.append({
+            'group_name': group.name or f"SuggestedGroup {group.suggestedgroup_id}",
+            'project_title': project_title,
+            'students': [
+                {'name': m.student.name, 'email': getattr(m.student, 'email', '')} 
+                for m in group.members.all()
+            ]
+        })
+
+    json_data = json.dumps(data, indent=2)
+
+    filename = f"suggested_groups_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
+
+    response = HttpResponse(json_data, content_type='application/json')
     response['Content-Disposition'] = f'attachment; filename="{filename}"'
     return response
